9ccca22c5b4e25b0e2cf98879edb89a1ac8d8299
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-10 00:40:55 +0000

    Added completed loaded-die.js file

---
+++ loaded-die.js
@@ -0,0 +1,18 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var listIndex = -1;
+
+  return function() {
+    /* your code here */
+    listIndex++;
+    if (listIndex === list.length) listIndex = 0;
+    return list[listIndex];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
